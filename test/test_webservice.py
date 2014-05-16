d03af8510b740700870c59d3511fef45bdd89732
D Tucker <danielle@tuckerlabs.com>
2014-05-15 22:46:42 -0700

    Test all actions in webservice.

--- test/test_webservice.py
+++ test/test_webservice.py
@@ -46,6 +46,15 @@ def test_root_get(test_app):
     for expected in expected_list:
         assert expected in data['boil']
 
+def test_echo_post(test_app):
+    resp = test_app.post('/echo', content_type='application/json', data=json.dumps({'first_param':'foo','second_param': 5 }))
+    assert resp.status_code == 200
+    assert resp.headers['Content-Type'] == 'application/json'
+    data = json.loads(resp.data)
+    assert data['content_type'] == 'application/json'
+    assert data['content']['first_param'] == 'foo'
+    assert data['content']['second_param'] == 5
+
 @pytest.mark.parametrize(
     ['controller', 'expected', 'status'],
     [
@@ -113,13 +122,56 @@ def test_controller_get(test_app, controller, expected, status):
         'args'].iteritems():
             assert data[key] == value
 
-@pytest.mark.skipif(True, reason='Not implemented yet')
+# @pytest.mark.skipif(True, reason='Not implemented yet')
 @pytest.mark.parametrize(
     ['controller', 'input', 'error'],
     [
-        ['boil', {'interval': 5, 'dead_interval': 2000, 'duty': 0.5}, None],
-        ['boil', {'interval': -3}, {'status_code': 400, 'error': 'Bad juju'}]
+        # Happy Path
+        ['boil', {'interval': 5, 'duty': 0.5}, None],
+        # If additional things are passed in, they don't 'take'
+        ['boil', {'interval': 5, 'min_interval':20}, None],
+        # If throw error for invalid values
+        ['boil', {'interval': -3}, {'status_code': 400, 'error': 'interval must be between 1 and 10, inclusive'}],
+        # If second param is invalid, throw error
+        ['boil', {'interval': 5, 'duty': 2}, {'status_code': 400, 'error': 'duty cycle must be between 0 and 1, inclusive'}],
+        ['faker_fakey', None, {'status_code':404, 'error': 'controller faker_fakey not found'}]
     ]
 )
 def test_controller_post(test_app, controller, input, error):
-    pass
+    checklist = ['name','interval','min_interval','max_interval','duty','dead_interval', 'class']
+    before = test_app.get('/{}'.format(controller))
+    before_data = json.loads(before.data)
+
+    resp = test_app.post('/{}'.format(controller), content_type='application/json', data=json.dumps(input))
+    assert resp.headers['Content-Type'] == 'application/json'
+    data = json.loads(resp.data)
+
+    after = test_app.get('/{}'.format(controller))
+    after_data = json.loads(after.data)
+
+    if error:
+        assert resp.status_code == error['status_code']
+        assert data['error'] == error['error']
+        # With error, no changes happened!
+        for key in checklist:
+            if error['status_code'] == 404: 
+                break
+            assert before_data[key] == after_data[key]
+    else:
+        assert resp.status_code == 200
+        for key in checklist:
+            if key in ['duty', 'interval'] and key in input:
+                assert data['old'][key] == before_data[key]
+                assert data['new'][key] == input[key]
+                assert data['new'][key] == after_data[key]
+            else:
+                assert before_data[key] == after_data[key]
+
+def test_jason_io_content_type_error(test_app):
+    resp = test_app.post('/echo', data = '{}')
+    assert resp.status_code == 405
+    assert resp.headers['Content-Type'] == 'application/json'
+    data = json.loads(resp.data)
+    assert data['error'] == 'Content-type must be application/json, not '
+
+
